03e44df68cef5d5e0266c7c2d6bb82d45fc959fe
Anastasia <zaytseva7821@ya.ru>
2022-09-07 20:10:36 +0400

    Changed 1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -3,13 +3,6 @@
 // 12821 -> да
 // 23432 -> да
 
-int MakePositive(int arg)
-{
-    if (arg <0)
-        arg = arg * -1;
-return arg;
-}
-
 int CheckNumber(int arg)
 {
     if (arg < 10000 || arg > 99999)
@@ -25,20 +18,26 @@ int CheckNumber(int arg)
 
 System.Console.WriteLine("Введите пятизначное число");
 int num = Convert.ToInt32(Console.ReadLine());
-num = MakePositive(num);
-num = CheckNumber(num);
-if (num/10000 == num%10)
+if (num<0)
 {
-    if ((num-num/10000*10000)/1000 == (num/10%10))
+    System.Console.WriteLine("Отрицательное число не может считаться палиндромом");
+}
+else
+{
+    num = CheckNumber(num);
+    if (num/10000 == num%10)
     {
-        System.Console.WriteLine("Это палиндром");
+        if ((num-num/10000*10000)/1000 == (num/10%10))
+        {
+            System.Console.WriteLine("Это палиндром");
+        }
+        else
+        {
+            System.Console.WriteLine("Это не палиндром");
+        }
     }
     else
     {
         System.Console.WriteLine("Это не палиндром");
     }
-}
-else
-{
-    System.Console.WriteLine("Это не палиндром");
 }
